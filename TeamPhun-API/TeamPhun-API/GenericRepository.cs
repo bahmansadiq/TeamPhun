81fa7904bb4ff0cdaa596c959e1614c67d6b4f9e
blargo1 <bennie.largo@gmail.com>
2016-12-07 12:36:03 -0800

    Resolve conflicts

---
+++ TeamPhun-API/TeamPhun-API/GenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamPhun_API
+{
+    public class GenericRepository
+    {
+    }
+}
---
+++ TeamPhun-API/TeamPhun-API/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamPhun_API.Interfaces
+{
+    interface IGenericRepository
+    {
+        //The IGenericRepository interface is a generic interface that defines the same set of methods as before. Notice that this time instead of Customer entity it uses T everywhere. Also notice that SelectByID() and Delete() methods now accept object parameter instead of string. This is necessary because different tables may have different types of primary keys (Customers table has a string primary key whereas Employees table has an integer primary key).
+        IEnumerable<T> SelectAll();
+        T SelectByID(object id);
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(object id);
+        void Save();
+    }
+}
