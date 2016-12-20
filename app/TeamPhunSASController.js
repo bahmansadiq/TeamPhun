f6792ce1affb599fc415afca00c85d0692e93e4f
Marci Tarpy <marcitarpy@gmail.com>
2016-12-20 12:31:10 -0800

    TeamPhun SAS Contrller adn HTML from Tristian and Bennie

--- app/TeamPhunSASController.js
+++ app/TeamPhunSASController.js
@@ -1,67 +1,38 @@
 (function() {
     'use strict';
-
     angular
         .module('app')
         .controller('TeamPhunSASController', TeamPhunSASController);
-
-    TeamPhunSASController.$inject = ['TeamPhunSASFactory', '$q', 'filterFilter'];
-
+    TeamPhunSASController.$inject = ['TeamPhunSASFactory', '$q', 'filterFilter', '$timeout'];
     /* @ngInject */
-    function TeamPhunSASController(TeamPhunSASFactory, $q, filterFilter) {
+    function TeamPhunSASController(TeamPhunSASFactory, $q, filterFilter, $timeout) {
         var vm = this;
         vm.title = 'TeamPhunSASController';
         vm.getCategory = getCategory;
         vm.getSpecs = getSpecs;
-
         // vm.getProducts = getProducts;
         vm.findStyleByCatId = findStyleByCatId;
-        vm.getPriceByStyleId = getPriceByStyleId
         vm.styleIdQtyCasePrice = [];
         vm.priceList = [];
-
-       
-      //  vm.styleDetails=[];
-        var styles=[];
-
-
-
-
-
         //  vm.styleDetails=[];
-        // var styles = [];
-
-        // var stylesImg = [];
-
-
-        //vm.categoryId=categoryId;
-
+        vm.itemStyleByCatId = [];
         activate();
-
-
-
-
         ////////////////
-        /**/
         function activate() {
             // getProducts();
             getSpecs();
             getCategory();
-
         }
 
         function getCategory() {
             TeamPhunSASFactory.getCategory().then(function(response) {
-
                 var array = response.data;
                 var array2 = [];
-
                 for (var i = 0, Sas = array.length; i < Sas; i++) {
                     // console.log(array[i]);
                     array2.push(array[i]);
                     // console.log(array2);
                 }
-
                 vm.data = array2;
             })
         }
@@ -72,81 +43,48 @@
             })
         }
 
-        // function getProducts() {
-        //     TeamPhunSASFactory.getProducts().then(function(response) {
-        //         var findPrice = response.data;
-        //         var styleId = response.data.styleID;
-        //         var casePrice = response.data.casePrice;
-        //         var quantity = response.data.qty;
-
-        //         for (var i = 0; i < findPrice.length; i++) {
-        //             var price = findPrice[i].styleId;
-        //             //  console.log(priceArray[i]);
-        //             vm.priceList.push(findPrice[i]);
-        //             vm.styleIdQtyCasePrice.push(price);
-        //             //console.log(priceArray2);
-        //         }
-
-
-
-
-
-        //         // console.log("its passing")
-
-        //     })
-        // }
-
         function findStyleByCatId(categoryId) {
-            TeamPhunSASFactory.getStyleByCatId(categoryId).then(function(response) {
-                vm.styleByCatId = response.data;
-
-                var filteredResult = filterFilter(vm.styleByCatId, { categories: categoryId });
-                vm.itemStyleByCatId = [];
-
-                for (var i = 0; i < filteredResult.length; i++) {
-                    var somethingObject= {brandName: filteredResult[i].brandName, title: filteredResult[i].title, styleImage: filteredResult[i].styleImage};
-                    vm.itemStyleByCatId.push(somethingObject);
-                    console.log(vm.itemStyleByCatId)
-                }
-
-                console.log(vm.itemStyleByCatId)
-                    // var len = response.data;
-
-                // //vm.test= response.data;
-                // for (var j = 0; j < len.length; j++) {
-                //     var item = len[j].categories.split(',');
-                //     var style = len[j];
-                //     var itemStyleId = len[j].styleID;
-
-                //     for (var t = 0; t < item.length; t++) {
-
-                //         if (item[t] == categoryId) {
-                //             vm.itemStyleByCatId.push(style);
-
-                //         };
-                //         if (itemStyleId == style.styleID) {
-                //             TeamPhunSASFactory.getProducts(style.styleID)
-                //                 .then(function(result) {
-                //                     vm.getStyleIdPrice = result.data;
-                //                      vm.itemStyleByCatId[j].push(vm.getStyleIdPrice[j]);
-                //                 });
-                //         }
-
-
-                //     }
-
-
-
-                // }
-            })
-
+            activate();
+            // 1. Get styles
+            TeamPhunSASFactory.getStyleByCatId() //TODO: rename this to .getStyles as input is not required
+                .then(function(response) {
+                    // 2. Store styles in vm
+                    vm.styleByCatId = response.data; // TODO: vm.styleByCatId can be renamed vm.styles
+                    // 3. Only get styles for the given category
+                    var filteredResult = filterFilter(vm.styleByCatId, { categories: categoryId });
+                    // 4. Push each of those styles into the array
+                    // 5. But also (and here's the clincher) - we need to get casePrice for each style.
+                    for (var i = 0; i < filteredResult.length; i++) {
+                        var style = {
+                            brandName: filteredResult[i].brandName,
+                            title: filteredResult[i].title,
+                            styleImage: filteredResult[i].styleImage,
+                            styleID: filteredResult[i].styleID,
+                            casePrice: vm.productPrice
+                        };
+                        (function(style) {
+                            $timeout(function() {
+                                TeamPhunSASFactory
+                                    .getProducts(style.styleID)
+                                    .then(function(response) {
+                                        var products = response.data;
+                                        var set = [];
+                                        products.forEach(function(p) {
+                                            set.push(p.caseQty);
+                                        })
+                                        products.forEach(function(p) {
+                                            set.push(p.casePrice);
+                                        });
+                                        style.averagePrice = set.reduce(function(x, y) {
+                                            return x + y;
+                                        }) / set.length;
+                                    });
+                            }, i * 1500);
+                        })(style);
+                        vm.itemStyleByCatId.push(style);
+                    }
+                })
+            vm.styleIdQtyCasePrice = [];
         }
-
-        function getPriceByStyleId() {
-
-
-        }
-
     }
-
 })();
